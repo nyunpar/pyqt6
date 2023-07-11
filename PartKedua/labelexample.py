78e5e2e81372156532050d1a8bae74b9ec6a6902
Nirwan <ny.unpar@gmail.com>
2023-07-11 12:59:37 +0700

    qlabel

---
+++ PartKedua/labelexample.py
@@ -0,0 +1,36 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtGui import QIcon, QFont, QPixmap, QMovie
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(200, 200, 700, 400)
+        self.setWindowTitle("PyQt6 Window")
+        self.setWindowIcon(QIcon("images/python.png"))
+
+        '''label = QLabel('Python GUI Development', self)
+        label.setText('New Text is Here')
+        label.move(100, 100)
+        label.setFont(QFont("Sanserif", 15))
+        label.setStyleSheet('color:red')
+
+        # label.setText(str(12))
+        label.setNum(15)
+        label.clear()
+        label = QLabel(self)
+        pixmap = QPixmap('images/python.png')
+        label.setPixmap(pixmap)
+        '''
+        label = QLabel(self)
+        movie = QMovie('images/sky.gif')
+        label.setMovie(movie)
+        movie.start()
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
